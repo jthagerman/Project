e7b32274fcda200190a2da3b4b2caf61fe900f67
jthagerman <jthagerma@gmail.com>
2023-01-15 20:11:03 -0500

    feat/RecommendedPart: finished basic component

---
+++ Components/Functional/RecommendedPart/index.tsx
@@ -0,0 +1,43 @@
+import {
+  Container,
+  Thumbnail,
+  Name,
+  Description,
+  Price,
+  Buttons,
+} from "@/Components/Styled/RecommendedPart";
+import Button from "../Button";
+import { useTheme } from "styled-components";
+import { UITheme } from "@/types";
+
+export default function RecommendedPart({
+  name,
+  price,
+  thumbnail,
+  description,
+}: {
+  name: string;
+  price: string;
+  thumbnail: string;
+  description: string;
+}) {
+  const theme = useTheme() as UITheme;
+  return (
+    <Container>
+      <Thumbnail src={thumbnail} />
+      <Name>{name}</Name>
+      <Description>{description + description}</Description>
+      <Price>{price}</Price>
+      <Buttons>
+        <Button
+          fontSize="1.125rem"
+          color={theme.colors.blackFont}
+          bgColor={theme.colors.white}
+        >
+          Read More
+        </Button>
+        <Button fontSize="1.125rem">Buy Now</Button>
+      </Buttons>
+    </Container>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,42 @@ import Head from "next/head";
 import Button from "@/Components/Functional/Button";
 import Icon from "@/Components/Functional/Icon";
 import tuning from "public/images/icons/tuning.svg";
+import RecommendedPart from "@/Components/Functional/RecommendedPart";
+import { faker } from "@faker-js/faker";
+import dynamic from "next/dynamic";
 
-export default function Home({ time }: any) {
+// import { faker } from '@faker-js/faker/locale/de';
+
+export default function Home({ time, parts }: any) {
   return (
     <>
       <Head>
         <title>Project</title>
       </Head>
 
-      <div></div>
+      <div>
+        <RecommendedPart {...parts} />
+      </div>
     </>
   );
 }
 
 export function getStaticProps() {
   console.log("Getting Static Props");
+
+  function createReccomendedPart() {
+    return {
+      name: faker.commerce.product(),
+      price: faker.commerce.price(100, 200, 2, "$"),
+      description: faker.commerce.productDescription(),
+      thumbnail: faker.image.technics(214, 214, false),
+    };
+  }
+
   return {
     props: {
       time: new Date().getTime(),
+      parts: createReccomendedPart(),
     },
     revalidate: 100,
   };
