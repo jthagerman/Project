e2bb2356d0097aa028d71436658fb5ec9cc646a9
jthagerman <jthagerma@gmail.com>
2023-01-30 15:51:24 -0500

    feat/headings: increased headings tags  by 1

--- Components/Styled/Heading/index.tsx
+++ Components/Styled/Heading/index.tsx
@@ -4,14 +4,14 @@ export const Container = styled.section<{ margin: string }>`
   text-align: center;
   margin: ${(props) => props.margin};
 `;
-export const Heading = styled.h1`
+export const Heading = styled.h2`
   margin: auto auto 0.25rem auto;
   font-family: ${(props) => props.theme.fonts.names.outfit};
   @media (max-width: ${(props) => props.theme.breakpoints.largePhone}) {
     font-size: 2rem;
   }
 `;
-export const Subheading = styled.h2`
+export const Subheading = styled.h3`
   margin: auto;
   color: ${(props) => props.theme.colors.gray};
   font-size: 1rem;
